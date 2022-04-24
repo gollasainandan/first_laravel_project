4de2045b8bdaf8ba4c06de3d39ccf1ecdfa21568
gollasainandan <sainandan1995@gmail.com>
2022-04-24 21:16:09 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+use App\Http\Controllers\EmployeeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [EmployeeController::class,'index']);
+Route::post('/store', [EmployeeController::class, 'store'])->name('store');
+Route::get('/fetch-all', [EmployeeController::class, 'fetchAll'])->name('fetchAll');
+Route::get('/edit', [EmployeeController::class, 'editEmployee'])->name('editEmployee');
+Route::post('/update', [EmployeeController::class, 'updateEmployee'])->name('updateEmployee');
+Route::delete('/delete', [EmployeeController::class, 'deleteEmployee'])->name('deleteEmployee');
